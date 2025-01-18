25efa973b4aec98cbb841d1f8eca7385e02d58cb
wangliying-2006 <2556695694@qq.com>
2025-01-18 09:57:06 +0800

    1

--- JavaScript/3.js
+++ JavaScript/3.js
@@ -1,9 +1,9 @@
 let students = [
-    { name: '张三', age: 18, score: 85 },
-    { name: '李四', age: 19, score: 92 },
-    { name: '王五', age: 18, score: 78 },
-    { name: '赵六', age: 20, score: 90 },
-    { name: '孙七', age: 19, score: 88 }
+    { name: '小李', age: 18, score: 85 },
+    { name: '小王', age: 19, score: 92 },
+    { name: '小苏', age: 18, score: 78 },
+    { name: '小吴', age: 20, score: 90 },
+    { name: '小陈', age: 19, score: 88 }
 ];
 
 function calculateAverageScore(students) {
